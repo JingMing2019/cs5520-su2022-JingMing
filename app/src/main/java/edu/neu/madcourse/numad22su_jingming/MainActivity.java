6ecd3e82a2efe3900d20a3fe782f9f4775814acd
jingming <ming.j@northeastern.edu>
2022-05-26 17:38:10 -0700

    Change the onClick method of about me button

--- app/src/main/java/edu/neu/madcourse/numad22su_jingming/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_jingming/MainActivity.java
@@ -8,28 +8,27 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button button;
+public class MainActivity extends AppCompatActivity{
+    private Button aboutMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.about_me_button);
+        aboutMeButton = findViewById(R.id.about_me_button);
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameAndEmail = "Jing Ming: ming.j@northeastern.edu";
+                int duration = Toast.LENGTH_SHORT;
 
-        button.setOnClickListener(this);
+                Toast toast = Toast.makeText(MainActivity.this, nameAndEmail, duration);
+                toast.show();
+            }
+        });
 
         TextView textView = findViewById(R.id.helloID);
         textView.setText(R.string.hello_world_string);
-    }
-
-    @Override
-    public void onClick(View v) {
-        String nameAndEmail = "Jing Ming: ming.j@northeastern.edu";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(MainActivity.this, nameAndEmail, duration);
-        toast.show();
     }
 }

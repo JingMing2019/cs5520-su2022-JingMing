d61042967e115f58689457802c6f3a7e295714fc
jingming <ming.j@northeastern.edu>
2022-05-26 22:24:43 -0700

    add activity click layout

--- app/src/main/java/edu/neu/madcourse/numad22su_jingming/ClickyActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_jingming/ClickyActivity.java
@@ -9,5 +9,6 @@ public class ClickyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky);
     }
 }

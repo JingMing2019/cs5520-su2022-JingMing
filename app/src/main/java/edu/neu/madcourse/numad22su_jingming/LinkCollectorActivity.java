d3c8d0c0e7c604cfb84adccd992942f7a7b8d9b5
jingming <ming.j@northeastern.edu>
2022-06-01 16:40:39 -0700

    change the fab color and add click fab logic

--- app/src/main/java/edu/neu/madcourse/numad22su_jingming/LinkCollectorActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_jingming/LinkCollectorActivity.java
@@ -1,14 +1,26 @@
 package edu.neu.madcourse.numad22su_jingming;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LinkCollectorActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class LinkCollectorActivity extends AppCompatActivity implements View.OnClickListener{
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_collector);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.fab_link_collector_ID) {
+            Snackbar.make(v, "Link creation successful", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
     }
 }

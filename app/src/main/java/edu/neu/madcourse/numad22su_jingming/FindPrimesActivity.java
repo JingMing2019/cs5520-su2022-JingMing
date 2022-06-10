e54052a639a480ee36b91b0a0d8f1609166f1230
jingming <ming.j@northeastern.edu>
2022-06-10 11:25:44 -0700

    make comment

--- app/src/main/java/edu/neu/madcourse/numad22su_jingming/FindPrimesActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_jingming/FindPrimesActivity.java
@@ -3,8 +3,6 @@ package edu.neu.madcourse.numad22su_jingming;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -14,6 +12,7 @@ import android.widget.TextView;
 
 public class FindPrimesActivity extends AppCompatActivity {
     private static final String TAG = "FindPrimesActivity";
+    private final Handler textHandler = new Handler(Looper.getMainLooper());
     TextView foundPrimeTV;
     TextView checkedNumTV;
     private boolean isTerminate;
@@ -27,6 +26,8 @@ public class FindPrimesActivity extends AppCompatActivity {
         isTerminate = true;
     }
 
+    // find primes button onClickListener. If there's no prime searching running, starts a new
+    // runnable Thread
     public void findPrimes(View view) {
         if (!isTerminate) {
             return;
@@ -36,10 +37,13 @@ public class FindPrimesActivity extends AppCompatActivity {
         new Thread(runnableThread).start();
     }
 
+    // Terminate button onClickListener. Stop the thread.
     public void terminate(View view) {
         isTerminate = true;
     }
 
+    // While prime searching is running, click back button brings up a dialog to ask user to confirm
+    // whether they want finish searching or not.
     @Override
     public void onBackPressed() {
         if(!isTerminate) {
@@ -58,12 +62,18 @@ public class FindPrimesActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This class represents a runnable thread. When this thread starts, it will check the number
+     * from 2 and increment it by 1 to look for prime numbers while no terminate action interrupts.
+     * A textHandler is used to display the latest found prime number and the current being checked
+     * number.
+     */
     class runnableThread implements Runnable {
         public void run() {
             int i = 2;
             while(!isTerminate) {
                 final int finalI = i;
-                new Handler(Looper.getMainLooper()).post(() -> {
+                textHandler.post(() -> {
                     checkedNumTV.setText(R.string.current_check_num_string);
                     checkedNumTV.append(String.valueOf(finalI));
                     if (isPrime(finalI)) {
@@ -73,8 +83,9 @@ public class FindPrimesActivity extends AppCompatActivity {
                 });
                 Log.v(TAG, "Runnable tread: " + i);
                 try {
-                    // 20 times per second
-                    Thread.sleep(50); //Makes the thread sleep or be inactive for 0.05 seconds
+                    // Makes the thread sleep or be inactive for 0.05 seconds. In total, the thread
+                    // runs 20 times per second.
+                    Thread.sleep(50); //
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -83,6 +94,8 @@ public class FindPrimesActivity extends AppCompatActivity {
         }
     }
 
+    // Determine whether a `num` is prime or not. Using naive prime searching algorithm. If `num`
+    // has a factor in [2, num - 1], means it is not a prime.
     private boolean isPrime(int num) {
         for(int i = 2; i < num; i++) {
             if(num % i == 0) {

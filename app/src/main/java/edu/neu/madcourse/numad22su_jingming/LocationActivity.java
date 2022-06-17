7738a5fb8a389bfbf92076f1f61cc20cc675b00a
jingming <ming.j@northeastern.edu>
2022-06-16 20:52:44 -0700

    Create layout for LocationActivity

--- app/src/main/java/edu/neu/madcourse/numad22su_jingming/LocationActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_jingming/LocationActivity.java
@@ -9,5 +9,6 @@ public class LocationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
     }
 }
